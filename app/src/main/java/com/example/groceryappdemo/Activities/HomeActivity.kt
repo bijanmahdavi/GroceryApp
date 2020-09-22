6f1c0bec180b934acf23317f08691d0fbbe9b3a2
Bijan Mahdavi <bijmahdavi@gmail.com>
2020-09-22 07:32:56 -0700

    custom action bar functionality. Need to polish

--- app/src/main/java/com/example/groceryappdemo/Activities/HomeActivity.kt
+++ app/src/main/java/com/example/groceryappdemo/Activities/HomeActivity.kt
@@ -2,6 +2,9 @@ package com.example.groceryappdemo.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
@@ -13,6 +16,7 @@ import com.example.groceryappdemo.R
 import com.example.groceryappdemo.app.Endpoints
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.app_tool_bar.*
 
 class HomeActivity : AppCompatActivity() {
     private var mList: ArrayList<CategoryDataItem> = ArrayList()
@@ -23,7 +27,31 @@ class HomeActivity : AppCompatActivity() {
         init()
     }
 
+    private fun setupToolBar() {
+        var toolbar = toolbar
+        toolbar.title = "Welcome"
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> finish()
+            R.id.action_cart -> Toast.makeText(applicationContext, "Shopping Cart", Toast.LENGTH_SHORT).show()
+            R.id.action_settings -> Toast.makeText(applicationContext, "Settings", Toast.LENGTH_SHORT).show()
+            R.id.action_profile -> Toast.makeText(applicationContext, "Profile", Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+
+
     private fun init() {
+        setupToolBar()
         getData()
         adapterImg = CategoryAdapter(this, mList)
         recycler_view_home.layoutManager = GridLayoutManager(this, 2)
